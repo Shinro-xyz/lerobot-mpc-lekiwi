527187f2335353b129148e8d4881861493559de3
Adil Faisal <adilfaisal01@proton.me>
2026-06-17 17:00:02 -0400

    feat: implement discrete-time LQR controller class

--- lqr.py
+++ lqr.py
@@ -1,2 +1,7 @@
 import numpy as np
 from components import Controller
+from scipy.linalg import solve_discrete_are
+
+class LQR(Controller):
+    def __init__(self) -> None:
+        super().__init__()
