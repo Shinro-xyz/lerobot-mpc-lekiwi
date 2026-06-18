1e15eba2fab91032c9aa2eac3e7300951d33c5f7
Adil Faisal <adilfaisal01@proton.me>
2026-06-18 16:00:03 -0400

    feat: add rotation axes parameter and implement forward kinematics

--- armrobot.py
+++ armrobot.py
@@ -2,12 +2,13 @@ import numpy as np
 from components import Plant
 
 class ArmRobot(Plant):
-    def __init__(self, num_dof:int, dt:float, joint_limits:np.ndarray,joint_offsets:np.ndarray):
+    def __init__(self, num_dof:int, dt:float, joint_limits:np.ndarray,joint_offsets:np.ndarray, rot_axes: list[str]):
         self.num_dof=num_dof
         self.dt=dt
         self.state=np.zeros(num_dof)
         self.joint_offsets=joint_offsets
         self.joint_limits=joint_limits
+        self.axes=rot_axes
 
     def get_state(self):
         return self.state.copy()
@@ -16,11 +17,11 @@ class ArmRobot(Plant):
         A=np.eye(self.num_dof)
         B=self.dt*np.eye(self.num_dof)
         return A,B
-    def _homogenous_transform(self,joint_angles:np.ndarray,axes:list[str]):
+    def _homogenous_transform(self,joint_angles:np.ndarray):
         sines,cosines= np.sin(joint_angles), np.cos(joint_angles)
         T=np.zeros((self.num_dof,4,4))
         for i in range(self.num_dof):
-            axis=axes[i]
+            axis=self.axes[i]
             s,c=sines[i], cosines[i]
             if axis=='x':
                 R=np.array([[1,0,0],[0,c,-s],[0,s,c]])
@@ -30,10 +31,19 @@ class ArmRobot(Plant):
                 R=np.array([[c,-s,0],[s,c,0],[0,0,1]])
             else:
                 raise ValueError(f"Invalid axis '{axis}' at joint {i}. Choose 'x', 'y', or 'z'.")
-            offset_vector= self.joint_offsets[i,:3].reshape(3,1)
-            T[:3,:3]=R
-            T[:3,3]=offset_vector
-            T[3,3]=1.0
+            offset_vector= self.joint_offsets[i,:3]
+            T[i,:3,:3]=R
+            T[i,:3,3]=offset_vector
+            T[i,3,3]=1.0
         return T
     def forward_kinematics(self,joint_angles:np.ndarray):
+        T_joints= self._homogenous_transform(joint_angles)
+        T_cumulatative=np.eye(4)
+
+        for i in range(self.num_dof):
+            T_cumulatative=T_cumulatative@T_joints[i]
+
+        return T_cumulatative
+
+    def _jacobian(self,)
         
